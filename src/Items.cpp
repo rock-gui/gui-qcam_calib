3b85eadc66b5b106ca815b99aea5a9f684db73ac
Alexander Duda <Alexander.Duda@dfki.de>
2014-02-20 09:16:08 +0100

    opencv backward compatibility fix

--- src/Items.cpp
+++ src/Items.cpp
@@ -1,8 +1,8 @@
-#include <opencv2/core.hpp>
-#include <opencv2/calib3d.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/contrib.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/contrib/contrib.hpp>
 
 #include <iostream>
 #include <stdexcept>
