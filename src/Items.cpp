f1ebabf52b0889034c48665e65a5e5cea53531ca
Alexander Duda <Alexander.Duda@dfki.de>
2014-02-20 09:17:41 +0100

    opencv backward compatibility fix
    Conflicts:
    	src/Items.cpp:q

--- src/Items.cpp
+++ src/Items.cpp
@@ -1,6 +1,7 @@
-#include <opencv2/core.hpp>
-#include <opencv2/calib3d.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 #include <iostream>
 
 #include "Items.hpp"
